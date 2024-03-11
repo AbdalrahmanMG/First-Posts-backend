6bfd8032a54fcff159712a3d8af0d7863793373d
AbdalrahmanMG <abdalrahman.m.gomaa@gmail.com>
2024-03-11 14:31:27 +0200

    comment category required

--- models/post.js
+++ models/post.js
@@ -24,7 +24,7 @@ const postSchema = mongoose.Schema({
   category: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "Category",
-    required: true,
+    // required: true,
   },
   dateCreated: {
     type: Date,
